695f11efb7a0f928ce1c0883d27f2dd08123880c
harrison <851596652@qq.com>
2019-01-04 14:49:53 +0800

    1

--- src/mixin/index.js
+++ src/mixin/index.js
@@ -1,4 +1,4 @@
-import { mapActions, mapState, mapGetters } from 'vuex'
+// import { mapActions, mapState, mapGetters } from 'vuex'
 export const mixinShow = {
   props: {
     show: {
@@ -17,38 +17,6 @@ export const mixinShow = {
     },
     selfShow (v) {
       this.$emit('update:show', v)
-    }
-  }
-}
-
-export const mixinLoginWerun = {
-  computed: {
-    ...mapState(['authWerun', 'isLogin', 'openId', 'todayCanChangeStep', 'todayAllStep'])
-  },
-  methods: {
-    ...mapActions(['LOGIN', 'AUTH_OF_WERUN', 'REPORT_OF_WERUN', 'FETCH_USER_INFO']),
-    async _getSteps () {
-      await this.LOGIN() // 登录
-      await this.AUTH_OF_WERUN() // 申请授权
-      await this.REPORT_OF_WERUN() // 上报微信运动数据给后台，返回是否成功的标志
-      await this.FETCH_USER_INFO() // 获取用户信息
-      await this._getRank()
-      // console.log('getSteps成功')
-    }
-  }
-}
-export const mixinaddressInfoSubmit = {
-  computed: {
-    ...mapGetters(['addressInfo', 'addressInfoErr'])
-  },
-  methods: {
-    async _checkErr () {
-      await this.$utils.rest.sleep(300) // 避免submit早于blur事件
-      if (this.addressInfoErr) {
-        this.utils.showToast(this.addressInfoErr)
-        return true
-      }
-      return false
     }
   }
 }
--- src/store/actions.js
+++ src/store/actions.js
@@ -2,29 +2,6 @@ import * as types from './mutation-types'
 import {pf} from '@/utils'
 import * as api from '@/http/api'
 import auths from '@/utils/auths'
-
-// async function reLogin (commit) {
-//   try {
-//     // console.log('登录')
-//     const loginResult = await pf('login') // 重新登录
-//     // console.log('loginResult', loginResult)
-//     try {
-//       const result = await api.login({ code: loginResult.code }) // 将code发送给后台获取openid
-//       commit(types.SET_IS_LOGIN, true)
-//       commit(types.SET_OPEN_ID, result.data.openId)
-//       // console.log('登录成功')
-//     } catch (e) {
-//       // console.log('获取openid失败', e)
-//       commit(types.SET_IS_LOGIN, false)
-//       return Promise.reject(new Error('【self.server】登录失败'))
-//     }
-//   } catch (e) {
-//     // console.log('登录失败', e)
-//     commit(types.SET_IS_LOGIN, false)
-//     return Promise.reject(new Error('【wx.server】登录失败'))
-//   }
-// }
-
 async function reLogin (commit) {
   try {
     const loginResult = await pf('login') // 重新登录
@@ -45,26 +22,6 @@ const actions = {
       }
     })
   },
-  // async LOGIN ({commit, state}) {
-  //   // console.log('LOGIN')
-  //   try {
-  //     await pf('checkSession')
-  //     // console.log('checkSessionResult', checkSessionResult)
-  //     if (!state.openId) { // 本地openid缓存被清
-  //       // console.log('本地openid缓存被清')
-  //       try {
-  //         await reLogin(commit) // 不让reLogin抛出异常，否则会执行下面的【2次登陆】
-  //       } catch (e) {
-  //         return Promise.reject(e)
-  //       }
-  //     } else {
-  //       // console.log('session有效且openId存在')
-  //     }
-  //   } catch (e) { // session过期
-  //     // console.log('session过期', e)
-  //     await reLogin(commit) // 【2次登录】
-  //   }
-  // },
   async LOGIN ({commit, state}) {
     wx.checkSession({
       success: async () => {
@@ -84,38 +41,6 @@ const actions = {
   async AUTH_OF_USER_INFO ({commit}) {
     const isAuthOfUI = await auths.auth('scope.userInfo')
     commit(types.SET_AUTH_USER_INFO, isAuthOfUI)
-  },
-  async REPORT_OF_WERUN ({commit, state}) {
-    if (state.isLogin && state.openId && state.authWerun) {
-      // console.log('【可以获取步数了哦哦哦哦】')
-      const {encryptedData, iv} = await pf('getWeRunData')
-      // console.log('ed', encryptedData)
-      // console.log('iv', iv)
-      await api.decrypt({
-        encryptedData,
-        iv,
-        openId: state.openId,
-        type: 'step'
-      })
-      return true
-    }
-    // console.log('未达到步数上报条件')
-    return false // 上传失败
-  },
-  async FETCH_USER_INFO ({commit, state}) {
-    const result = await api.getUserInfo({ openId: state.openId })
-    // console.log('获取到了用户信息', result.data)
-    commit(types.SET_USER_INFO, result.data)
-  },
-  async FETCH_ADVS ({commit}) {
-    const result = await api.getAdvs()
-    commit(types.SET_ADVS, result.data)
-    return result.data
-  },
-  async FETCH_RULE ({commit}) {
-    const result = await api.getRule()
-    // console.log('rule', result.data)
-    commit(types.SET_RULE, result.data)
   },
   async FETCH_FINANCIAL_CATEGORY_LIST ({commit}) {
     const result = await api.getFinancialCategory()
