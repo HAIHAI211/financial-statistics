5b54d9ce148e94fa26c40a988d1db01f2d2a67da
harrison <851596652@qq.com>
2019-01-07 11:42:18 +0800

    1

--- src/config.js
+++ src/config.js
@@ -2,8 +2,9 @@
 // const realHost = 'http://132.232.15.12:9701'
 // const hsHost = 'http://228i46u234.imwork.net/service-stepmath'
 // const realHost = 'http://localhost:8080/harrisonlanny'
-const realHost1 = 'http://192.168.31.183:8080/harrisonlanny'
-const host = realHost1
+// const realHost1 = 'http://192.168.31.183:8080/harrisonlanny'
+const tencentHost = 'http://139.199.35.20:8080/harrisonlanny'
+const host = tencentHost
 const appId = 'wx71086452be90ee8f'
 const appSecret = '981801959ef65a90fcf057c9edd97b6f'
 const config = {
